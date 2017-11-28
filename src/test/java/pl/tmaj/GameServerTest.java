ab97b65f74fc6f7455801e740c2ad3f50af8ddf9
Tomasz Maj <tmaj.pl@gmail.com>
2017-11-28 19:14:26 +0100

    test improvements and forward modeling

--- src/test/java/pl/tmaj/GameServerTest.java
+++ src/test/java/pl/tmaj/GameServerTest.java
@@ -4,41 +4,63 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 
-import static java.util.concurrent.Executors.newSingleThreadExecutor;
+import static java.util.concurrent.Executors.newFixedThreadPool;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GameServerTest {
 
-    private static final int PORT = 9191;
-    private ExecutorService gameServer; // ThreadLocal
+    private static final String HOST = "localhost";
+    private static final int DEFAULT_PORT = 9191;
+    private ThreadLocal<ExecutorService> executorService = new ThreadLocal<>();
 
     @BeforeEach
-    void initGame() {
-        gameServer = newSingleThreadExecutor();
-        gameServer.submit(GameServer::new);
+    void before() {
+        executorService.set(newFixedThreadPool(1));
+        executorService.get().submit(() -> new GameServer());
     }
 
     @AfterEach
-    void stopGame() {
-        gameServer.shutdown();
+    void after() {
+        executorService.get().shutdownNow();
     }
 
     @Test
-    void shouldListenOnPort9191() throws IOException {
-        assertTrue(isPortOpen(PORT));
+    void shouldListenOnDefaultPort() {
+        assertTrue(isGamePortOpen());
     }
 
     @Test
-    void shouldReleaseGateFor16Players() {
+    void shouldNotListenOnOtherPort() {
+        assertFalse(isGamePortOpen(8181));
+    }
+
+    @Test
+    void shouldStopListeningAfter16ThPlayer() {
+        connect16Players();
+        assertFalse(isGamePortOpen());
+    }
+
+    @Test
+    void shouldGenerateUniqueIdForEveryPlayer() {
         assertTrue(null);
     }
 
-    private static boolean isPortOpen(int port) {
-        try (Socket socket = new Socket("localhost", port)) {
+    private static void connect16Players() {
+        for (int i = 0; i < 16; i++) {
+            isGamePortOpen();
+        }
+    }
+
+    private static boolean isGamePortOpen() {
+        return isGamePortOpen(DEFAULT_PORT);
+    }
+
+    private static boolean isGamePortOpen(int port) {
+        try (Socket socket = new Socket(HOST, port)) {
             socket.close();
             return true;
         } catch (Exception e) {
