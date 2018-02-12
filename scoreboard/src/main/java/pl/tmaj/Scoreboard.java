da8eee1a51d6f71c284475712dd57e512f177c8e
Tomasz Maj <tmaj.pl@gmail.com>
2018-02-12 14:37:18 +0100

    integration with config server

--- scoreboard/src/main/java/pl/tmaj/Scoreboard.java
+++ scoreboard/src/main/java/pl/tmaj/Scoreboard.java
@@ -1,17 +1,23 @@
 package pl.tmaj;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class Scoreboard {
+
+    @Value("${welcome.message}")
+    private String welcomeMessage;
 
     @RequestMapping("/")
     @ResponseBody
     public String index() {
         // this service will request list from WinnersRepository
         // and display it in html5-like way
-        return "winners list";
+        return welcomeMessage;
     }
 }
