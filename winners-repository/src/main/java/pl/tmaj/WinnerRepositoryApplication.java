7c6550ebc24a6c4a617d9b835911fd577d82e660
Tomasz Maj <tmaj.pl@gmail.com>
2018-02-13 20:04:45 +0100

    removing default data

--- winners-repository/src/main/java/pl/tmaj/WinnerRepositoryApplication.java
+++ winners-repository/src/main/java/pl/tmaj/WinnerRepositoryApplication.java
@@ -1,12 +1,8 @@
 package pl.tmaj;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.stereotype.Component;
-
-import java.util.stream.Stream;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -14,23 +10,5 @@ public class WinnerRepositoryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WinnerRepositoryApplication.class, args);
-    }
-}
-
-@Component
-class DemoData implements CommandLineRunner {
-
-    private final WinnerRepository winnerRepository;
-
-    DemoData(WinnerRepository winnerRepository) {
-        this.winnerRepository = winnerRepository;
-    }
-
-    @Override
-    public void run(String... strings) throws Exception {
-        Stream.of("Josh", "Mark", "Elen", "Joe")
-                .forEach(name -> winnerRepository
-                        .save(new Winner(name))
-                );
     }
 }
