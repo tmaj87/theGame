7e98cee7f5610bb626bc2d79860461a7983584b6
Tomasz Maj <tmaj.pl@gmail.com>
2018-02-17 22:07:52 +0100

    Turbine baby steps

--- service-discovery/src/main/java/pl/tmaj/ServiceDiscoveryApplication.java
+++ service-discovery/src/main/java/pl/tmaj/ServiceDiscoveryApplication.java
@@ -3,9 +3,11 @@ package pl.tmaj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
-@EnableEurekaServer
 @SpringBootApplication
+@EnableEurekaServer
+@EnableHystrix
 public class ServiceDiscoveryApplication {
 
     public static void main(String[] args) {
