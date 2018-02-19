7b16663fa0763116fba5b846dbd890bb302bf785
Tomasz Maj <tmaj.pl@gmail.com>
2018-02-19 22:19:42 +0100

    staging for JavaServer and JavaClient

--- game-server/src/main/java/pl/tmaj/GameServerApplication.java
+++ game-server/src/main/java/pl/tmaj/GameServerApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-@EnableHystrix
+@EnableRetry
 public class GameServerApplication {
 
     public static void main(String[] args) {
