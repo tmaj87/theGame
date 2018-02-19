32ff6615fbf03f65a90ea8412acfa48398bcf9e2
Tomasz Maj <tmaj.pl@gmail.com>
2018-02-19 22:55:26 +0100

    cleanup and package movement

--- winner-repository/src/main/java/pl/tmaj/WinnerRepositoryApplication.java
+++ winner-repository/src/main/java/pl/tmaj/WinnerRepositoryApplication.java
@@ -3,11 +3,9 @@ package pl.tmaj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableHystrix
 public class WinnerRepositoryApplication {
 
     public static void main(String[] args) {
--- winner-repository/src/main/java/pl/tmaj/common/Winner.java
+++ winner-repository/src/main/java/pl/tmaj/common/Winner.java
@@ -1,4 +1,4 @@
-package pl.tmaj;
+package pl.tmaj.common;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
