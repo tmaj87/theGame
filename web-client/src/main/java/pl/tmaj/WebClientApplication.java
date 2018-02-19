40c778eeb4c6dbeb70b1d8e2b7f267cb226ccfd4
Tomasz Maj <tmaj.pl@gmail.com>
2018-02-19 22:17:43 +0100

    enable feign and retry

--- web-client/src/main/java/pl/tmaj/WebClientApplication.java
+++ web-client/src/main/java/pl/tmaj/WebClientApplication.java
@@ -3,9 +3,13 @@ package pl.tmaj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
+@EnableRetry
 public class WebClientApplication {
 
     public static void main(String[] args) {
