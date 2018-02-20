c7b2b392009f1a8af14dd6a93cb051c8b155c5dc
Tomasz Maj <tmaj.pl@gmail.com>
2018-02-20 10:22:19 +0100

    cleanup

--- service-discovery/src/main/java/pl/tmaj/ServiceDiscoveryApplication.java
+++ service-discovery/src/main/java/pl/tmaj/ServiceDiscoveryApplication.java
@@ -3,11 +3,9 @@ package pl.tmaj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaServer
-@EnableHystrix
 public class ServiceDiscoveryApplication {
 
     public static void main(String[] args) {
