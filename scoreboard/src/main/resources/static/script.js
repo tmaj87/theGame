8af32c63863a4d92032fb6a4feeb15ebfbc20ea7
Tomasz Maj <tmaj.pl@gmail.com>
2018-02-22 23:32:15 +0100

    bootstrapping

--- scoreboard/src/main/resources/static/script.js
+++ scoreboard/src/main/resources/static/script.js
@@ -1,10 +1,11 @@
 function obtainNewList() {
     $.ajax('/list')
         .done(function (data) {
-                let scoreboard = $('.scoreboard');
+                let scoreboard = $('#scoreboard');
                 scoreboard.html("");
                 for (let index in data) {
-                    scoreboard.append('<li>' + data[index] + '</li>')
+                    let place = parseInt(index) + 1;
+                    scoreboard.append('<tr class="id_' + place + '"><th scope="row">' + place + '</th><td>' + data[index] + '</td><td>1</td></tr>');
                 }
                 setTimeout(obtainNewList, 3000);
             }
