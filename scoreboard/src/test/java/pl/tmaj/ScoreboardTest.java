60a0307e7a4ca0b38bf4b2e94a6d795301e89cac
Tomasz Maj <tmaj.pl@gmail.com>
2018-08-13 22:19:07 +0200

    introducing scoreboard test cases

---
+++ scoreboard/src/test/java/pl/tmaj/ScoreboardTest.java
@@ -0,0 +1,33 @@
+package pl.tmaj;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ExecutorService;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.util.ReflectionTestUtils.setField;
+
+public class ScoreboardTest {
+
+    @Test
+    public void shouldGetLatestWinner() {
+
+    }
+
+    @Test
+    public void shouldGetSortedPlayersByPoints() {
+
+    }
+
+    @Test
+    public void shouldGetDefaultContentInCaseOfNoResponse() {
+
+    }
+
+    @Test
+    public void shouldGetDefaultContentInCaseOfNoWinners() {
+
+    }
+}
