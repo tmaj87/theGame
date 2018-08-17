116ad254a2c72a83de45f292e272e7ac6060e0d4
Tomasz Maj <tmaj.pl@gmail.com>
2018-08-18 00:05:36 +0200

    bounding scoreboard with tests

--- scoreboard/src/test/java/pl/tmaj/ScoreboardTest.java
+++ scoreboard/src/test/java/pl/tmaj/ScoreboardTest.java
@@ -1,24 +1,50 @@
 package pl.tmaj;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.hateoas.Resources;
 
-import java.util.concurrent.ExecutorService;
+import java.util.*;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.springframework.test.util.ReflectionTestUtils.setField;
+import static org.mockito.Mockito.when;
 
 public class ScoreboardTest {
 
+    private static final String DUMMY_PLAYER = "DUMMY_PLAYER";
+    private static final String DUMMY_PLAYER_1 = DUMMY_PLAYER + "_1";
+    private static final String DUMMY_PLAYER_2 = DUMMY_PLAYER + "_2";
+    private static final String DUMMY_PLAYER_3 = DUMMY_PLAYER + "_3";
+
+    private final WinnerRepository repository = mock(WinnerRepository.class);
+    private final Resources resources = mock(Resources.class);
+    private List<Winner> winners;
+    private Scoreboard scoreboard = new Scoreboard(repository);
+
+    @Before
+    public void setUp() {
+        winners = new ArrayList<>();
+        when(repository.getLatest()).thenReturn(resources);
+        when(resources.getContent()).thenReturn(winners);
+    }
+
     @Test
     public void shouldGetLatestWinner() {
+        addWinner(DUMMY_PLAYER_1);
+        addWinner(DUMMY_PLAYER_2);
+        addWinner(DUMMY_PLAYER_3);
+        String latest = scoreboard.getBestPlayer();
 
+        assertEquals(DUMMY_PLAYER, latest);
     }
 
     @Test
     public void shouldGetSortedPlayersByPoints() {
+        addWinner(DUMMY_PLAYER_1);
+        String latest = scoreboard.getBestPlayer();
 
+        assertEquals(DUMMY_PLAYER, latest);
     }
 
     @Test
@@ -29,5 +55,9 @@ public class ScoreboardTest {
     @Test
     public void shouldGetDefaultContentInCaseOfNoWinners() {
 
+    }
+
+    private void addWinner(String name) {
+        winners.add(new Winner(name));
     }
 }
