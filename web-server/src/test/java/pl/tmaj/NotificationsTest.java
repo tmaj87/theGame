77154926cfd02d9373295c44dd71ae508737e090
Tomasz Maj <tmaj.pl@gmail.com>
2018-08-28 23:06:34 +0200

    test

--- web-server/src/test/java/pl/tmaj/NotificationsTest.java
+++ web-server/src/test/java/pl/tmaj/NotificationsTest.java
@@ -3,6 +3,7 @@ package pl.tmaj;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.verification.VerificationMode;
 
 import java.util.concurrent.ExecutorService;
 
@@ -13,7 +14,7 @@ import static org.mockito.Mockito.*;
 
 public class NotificationsTest {
 
-    private ExecutorService executor = newFixedThreadPool(1);
+    private ExecutorService executor;
     private WebServer server;
     private UsersNotifier notifier;
     private Sleeper sleeper;
@@ -25,40 +26,46 @@ public class NotificationsTest {
         notifier = mock(UsersNotifier.class);
         sleeper = mock(Sleeper.class);
         notifications = new Notifications(server, notifier, sleeper);
+        executor = newFixedThreadPool(1);
         executor.execute(notifications);
     }
 
     @AfterEach
     void tearDown() {
+        notifications.stop();
         executor.shutdown();
     }
 
     @Test
     void shouldAskForMissingPlayers() {
-        verify(server).getMissingPlayers();
+        verify(server, onceDuringOneSecond()).getMissingPlayers();
     }
 
     @Test
     void shouldNotifyAboutMissingPlayers() {
-        verify(notifier, atLeastOnce()).notifyCount(anyInt());
+        verify(notifier, onceDuringOneSecond()).notifyCount(anyInt());
     }
 
     @Test
     void shouldSleepBetweenNotifications() throws Exception {
-        verify(sleeper, atLeastOnce()).sleep(anyInt());
+        verify(sleeper, onceDuringOneSecond()).sleep(anyInt());
     }
 
     @Test
     void shouldStopAfterInterruptedException() throws Exception {
         doThrow(InterruptedException.class).when(sleeper).sleep(anyInt());
 
-        verify(sleeper).sleep(anyInt());
+        verify(sleeper, onceDuringOneSecond()).sleep(anyInt());
 
         assertFalse(notifications.isRunning());
     }
 
     @Test
     void shouldPingWebServer() {
-        verify(server, atLeastOnce()).ping();
+        verify(server, onceDuringOneSecond()).ping();
+    }
+
+    private VerificationMode onceDuringOneSecond() {
+        return timeout(1_000).atLeast(1);
     }
 }
